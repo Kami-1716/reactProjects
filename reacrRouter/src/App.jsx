dbd4586ba28d74c43393249c20aaa8d88378a97d
Muhammad Kamran <134825430+Kami-1716@users.noreply.github.com>
2023-12-15 10:43:33 +0500

    Project Initiated for reactRouter

---
+++ reacrRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold bg-green-600 text-blue-900'>React Routing</h1>
+    </>
+  )
+}
+
+export default App
